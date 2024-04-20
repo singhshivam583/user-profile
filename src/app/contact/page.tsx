ece231c9425ba1d58c4fd2a76d7daeda3fbfa9e3
शिvam सिंह <singhshivam583@yahoo.in>
2024-04-20 11:15:05 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -22,7 +22,7 @@ function MusicSchoolContactUs() {
       <div className="max-w-2xl mx-auto p-4 relative z-10">
         {' '}
         {/* Add relative and z-10 to bring content to the front */}
-        <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-gray-300">
+        <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-gray-600">
           Contact Us
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
